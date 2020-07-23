2a13da2cd4ed315dae4a3c73f336b222c9758695
Kevin Safford <kevin@cognitops.com>
2020-07-23 12:11:49 -0500

    Adding more header content

--- bannedit.el
+++ bannedit.el
@@ -1,5 +1,34 @@
 ;;; bannedit.el --- Highlight banned words and summarily expel them.
 
+;; Copyright (C) 2020 by Kevin Safford
+;;
+;; Author: Ric Lister
+;; URL: https://github.com/ksafford/bannedit
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+;;
+;;; Commentary:
+;;
+;; This was inspired by thie post by Nat Eliason:
+;;  https://www.nateliason.com/blog/better-writer
+;; The idea is to highlight and commonly used weak words for easy
+;; identification
+
 ;; -*- lexical-binding: t -*-
 
 ;; TODO: Make the matching case insensitive
