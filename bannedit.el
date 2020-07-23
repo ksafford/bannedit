1922d52d8b77e04f2226bd46df87925fa87e9f41
Kevin Safford <kevin@cognitops.com>
2020-07-23 17:53:02 -0500

    Fix all checkdoc errors. Add lots of docstrings.

--- bannedit.el
+++ bannedit.el
@@ -12,7 +12,7 @@
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of the
+;; published by the Free Software Foundation; either version 3 of the
 ;; License, or any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful, but
@@ -33,30 +33,45 @@
 ;; identification
 
 
+;;; Code:
 ;; TODO: Edit the regex so "e.g." will match
 ;; TODO: Setup customize group to accept a face to use and the list of banned words
 
-(defun bannedit-phrase-to-case-insensitive-regex (phrase) ()
-       (mapconcat (lambda (c) (concat "\\(" (upcase (char-to-string c)) "\\|"  (char-to-string c) "\\)" )) phrase ""))
+(defun bannedit-phrase-to-case-insensitive-regex (phrase)
+  "Transform PHRASE into a regex string that will match PHRASE with any capitalization."
+  ()
+  (mapconcat (lambda (c) (concat "\\(" (upcase (char-to-string c)) "\\|"  (char-to-string c) "\\)" )) phrase ""))
 
-(defun bannedit-match-phrase-exactly (phrase) ()
-       (concat "\\b" (bannedit-phrase-to-case-insensitive-regex phrase)  "\\b"))
+(defun bannedit-match-phrase-exactly (phrase)
+  "Transform PHRASE into a regex string wrapped by word boundaries."
+  ()
+  (concat "\\b" (bannedit-phrase-to-case-insensitive-regex phrase)  "\\b"))
 
 (bannedit-match-phrase-exactly "therefore")
 
-(defun bannedit-highlight-exact-phrase-in-blue (p) ()
+(defun bannedit-highlight-exact-phrase-in-blue (p)
+  "Really just a partial function for 'highlight-phrase' taking only P, but with a FACE selected."
+  ()
        (highlight-phrase (bannedit-match-phrase-exactly p) 'anzu-match-1))
 
-(defun bannedit-highlight-banned-words (badwords) ()
-       (mapcar #'bannedit-highlight-exact-phrase-in-blue badwords))
+(defun bannedit-highlight-banned-words (badwords)
+  "Core function. Highlight all words in the list BADWORDS."
+  ()
+  (mapcar #'bannedit-highlight-exact-phrase-in-blue badwords))
 
-(defun bannedit-unhighlight (p) ()
-       (unhighlight-regexp (bannedit-match-phrase-exactly p)))
+(defun bannedit-unhighlight (p)
+  "Inverse of 'bannedit-highlight-exact-phrase'. Unhighlights occurrences of P."
+  ()
+  (unhighlight-regexp (bannedit-match-phrase-exactly p)))
 
-(defun bannedit-unhighlight-all (badwords) ()
-     (mapcar #'bannedit-unhighlight badwords))
+(defun bannedit-unhighlight-all (badwords)
+  "Inverse of 'bannedit-highlight-banned-words'. Unhighlights all phrases in BADWORDS."
+  ()
+  (mapcar #'bannedit-unhighlight badwords))
 
-(defun bannedit-toggle-bannedit () (interactive)
+(defun bannedit-toggle-bannedit ()
+  "Toggle highlighting of the bannedit-words list. In case you want to keep the mode active but remove or re-enable highlighting."
+  (interactive)
        (if bannedit-switch
            (progn
              (bannedit-unhighlight-all bannedit-words)
